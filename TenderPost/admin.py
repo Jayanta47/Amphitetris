989012c94c87eb38216c61e608772c13a4fd339c
Tanjim <tanjim.ak49@gmail.com>
2020-09-21 17:36:19 +0600

    fix vendor profile

--- TenderPost/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from TenderPost.models import *
-
-admin.site.register(TenderBid)
-admin.site.register(PurchaseOrder)
--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views as home_views
-app_name = 'home'
-
-urlpatterns = [
-    path('', home_views.homepage, name='main-page'),
-    path('about/', home_views.about_page, name='about')
-]
